028381052012ea3cf097134af1acf9dd37b7e1c3
Allen Zheng <az299@njit.edu>
2026-03-07 04:03:24 -0500

    Added 'doRegister' function to db listener

--- testRabbitMQServer.php
+++ testRabbitMQServer.php
@@ -12,14 +12,49 @@ function db() {
   }
 
   $connect = new mysqli('127.0.0.1', 'db_user', 'passwd123', 'auth_db');  // direct connection to our database
-
+  // kills this script if connection fails
   if($connect->connect_errno) {
     die("Database connections failed: " . $connect->connect_errno . PHP_EOL);
   }
   return $connect;
 }
 
-// make a function to register
+// function for account registration
+function doRegister($username, $password) {
+  $hashedPword = password_hash($password, PASSWORD_DEFAULT);
+  // prepping our query
+  $stmt = db()->prepare(                                      
+    "INSERT INTO users (username, pass_hash, acc_creation_time)
+     VALUES (?, ?, UNIX_TIMESTAMP())"
+  );
+  // checking to see if the prepping went wrong
+  if(!$stmt) {
+    return [
+      "success" => false,
+      "error" => "prep_failed"
+      ];
+  }
+  // attaching actual values to the placeholders
+  $stmt->bind_param("ss", $username, $hashedPword);
+  // a try catch statement for executing queries and catching what went wrong
+  // more specifically, seeing if there's a duplicate user
+  try{
+    $stmt->execute();
+  } catch(mysqli_sql_exception $e) {
+      if((int)$e->getCode() === 1062) {
+        return [
+          "success" => false,
+          "error" => "username_exists"
+        ];
+      }
+      return [
+        "success" => false,
+        "error" => "db_insert_failed"
+      ];
+  }
+  return ["success" => true];
+}
+
 // make a function to validate a session
 // make a function to logout
 
