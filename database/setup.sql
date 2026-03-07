369c0f01c539da178488ff98a95fa2c29a181b51
Allen Zheng <az299@njit.edu>
2026-03-07 01:41:39 -0500

    setup.sql and user.sql weren't added in the previous commits, now fixed

---
+++ database/setup.sql
@@ -0,0 +1,28 @@
+/*
+THIS FILE WILL MAKE OUR AUTHENTICATION DATABASE
+IF YOU WANT TO RUN THE AUTH DB ON YOUR VM FOR TESTING, MAKE SURE TO EXECUTE THIS FILE ALONG WITH "user.sql"
+*/
+
+-- making the db
+CREATE DATABASE auth_db;
+USE auth_db;
+
+-- making a table to store our users' information
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    pass_hash VARCHAR(255) NOT NULL,
+    acc_creation_time BIGINT NOT NULL -- epoch time
+);
+
+-- making a table to keep track of all the sessions
+CREATE TABLE sessions (
+    session_key CHAR(64) PRIMARY KEY,
+    user_id INT NOT NULL,
+    start_time BIGINT NOT NULL, -- epoch time
+    end_time BIGINT NOT NULL, --epoch time
+    -- linking this table with the users table so if a user is deleted from that table, their sessiosn will be delted too
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    INDEX (user_id),
+    INDEX (end_time)
+)
