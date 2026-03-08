a4383aeef1418e4284bd873bf793e18dacd3c812
Allen Zheng <az299@njit.edu>
2026-03-08 00:20:44 -0500

    fixed issue with lines 202 and 232, missing commas

--- db_listener.php
+++ db_listener.php
@@ -199,7 +199,7 @@ function requestProcessor($request)
   if(!isset($request['type']))
   {
     return [
-      "success" => false
+      "success" => false,
       "error" => "missing_type"
     ];
   }
@@ -229,7 +229,7 @@ function requestProcessor($request)
       );
     default:
       return [
-        "success" => false
+        "success" => false,
         "error" => "unsupported_type"
       ];
   }
