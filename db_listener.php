adb3a135b8a0f1e9c443a233e7d0485a0dd9f224
Allen Zheng <az299@njit.edu>
2026-03-08 01:07:46 -0500

    fixed unfinished statement resulting in syntax error, line 240 (db_listener)

--- db_listener.php
+++ db_listener.php
@@ -237,7 +237,7 @@ function requestProcessor($request)
 
 
 $server = new rabbitMQServer("testRabbitMQ.ini","testServer");
-echo "The database listener is now running and waiting for requests..."
+echo "The database listener is now running and waiting for requests..." . PHP_EOL;
 $server->process_requests('requestProcessor');
 exit();
 ?>
