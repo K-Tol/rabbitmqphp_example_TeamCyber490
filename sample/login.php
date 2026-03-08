9c6fdcfd52f275da2baf7294031f5a206453f204
Tom Paschalis <tp373@njit.edu>
2026-03-08 12:15:54 -0400

    added some rabbitmq require lines and register switch case

--- sample/login.php
+++ sample/login.php
@@ -1,22 +1,35 @@
 <?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
 try {
 	if (!isset($_POST)) {
 		$msg = "NO POST MESSAGE SET, POLITELY FUCK OFF";
 		echo json_encode($msg);
 		exit(0);
 	}
+	$client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
 	$request = $_POST['type'];
 	$username = $_POST['uname'];
 	$password = $_POST['pword'];
 	$response = "unsupported request type, politely FUCK OFF";
 	switch ($request) {
 		case "login":
-			$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
 			$client->send_request([
-				"type"=>"login", 
-				"username"=>$username, 
-				"password"=>$password]);
+				"type" => "login",
+				"username" => $username,
+				"password" => $password
+			]);
 			$response = "(testing) login, yeah we can do that";
+			break;
+		case "register":
+			$client->send_request([
+				"type" => "register",
+				"username" => $username,
+				"password" => $password
+			]);
+			$response = "registering...";
 			break;
 	}
 	echo json_encode($response);
