ae3f0946e0111abc2ab12f9008cfe235384f9a79
Allen Zheng <az299@njit.edu>
2026-03-09 22:02:24 -0400

    starting on the movie database

---
+++ database/setup_movieDB.sql
@@ -0,0 +1,11 @@
+/*
+THIS FILE WILL CREATE OUR MOVIE DATABASE
+EXECUTE THIS FILE WITH ALL THE OTHER .SQL FILES IN THIS FOLDER TO TEST OUT THE DB
+*/
+
+-- obj right now: give us the ability to browse/search movies and view movie profile pages
+
+CREATE DATABASE movie_db;
+USE movie_db;
+
+-- create table for storing movie info
