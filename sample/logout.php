ace5113121f5f57f6db5f06e994c65e32b9676a0
Tom Paschalis <tp373@njit.edu>
2026-03-10 11:03:10 -0400

    added logout logic to call listener logout to  remove session key when called

---
+++ sample/logout.php
@@ -0,0 +1,14 @@
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+$sessionKey = $_COOKIE["session_key"] ?? '';
+
+$client -> send_request([
+    "type" => "logout",
+    "session_key" => $sessionKey
+    ]);
+exit(0);
+?>
