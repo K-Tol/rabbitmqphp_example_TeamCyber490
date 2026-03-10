e1deca1f8c8f8f55779cea280871a361a7a25660
Allen Zheng <az299@njit.edu>
2026-03-10 01:05:26 -0400

    added movieDB function

--- moviedb_listener.php
+++ moviedb_listener.php
@@ -4,4 +4,26 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
+/*
+functions to make:
+-datasource client function
+-storing genres function
+-storing movies function
+-query for movie function
+-sum more
+*/
+
+// function for establishing connection with the movie database
+function movieDB() {
+    static $connect = null;
+    if($connect !== null) {
+        return $connect;
+    }
+
+    $connect = new mysqli('127.0.0.1', 'db_user', 'passwd123', 'movie_db');
+    if($connect->connect_errno) {
+        die("movie_db connection failed: " . $connect->connect_error . PHP_EOL);
+    }
+    return $connect;
+}
 ?>
