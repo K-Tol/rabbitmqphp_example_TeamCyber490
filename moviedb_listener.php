eaa251e0ab7cf65b9e9188f0760cd458e273835b
Allen Zheng <az299@njit.edu>
2026-03-10 21:07:47 -0400

    updated file name on line 246 and fixed spelling error on 247

--- moviedb_listener.php
+++ moviedb_listener.php
@@ -243,7 +243,7 @@ function requestProcessor($request) {
     }
 }
 
-$server = new rabbitMQServer("rabbitMQ.ini", "movieServer");
-echo "Movie database listener is now running..." . PHP_EOl;
+$server = new rabbitMQServer("movieServer.ini", "movieServer");
+echo "Movie database listener is now running..." . PHP_EOL;
 $server->process_requests('requestProcessor');
 ?>
