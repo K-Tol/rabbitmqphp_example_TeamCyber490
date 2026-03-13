719f72af0b431489af10b6d68744221b79699823
Allen Zheng <az299@njit.edu>
2026-03-13 17:44:26 -0400

    realized I added a function into the wrong file last night (brain stopped working at 3am), fixed it

--- moviedb_listener.php
+++ moviedb_listener.php
@@ -242,7 +242,22 @@ function getFullDetails($movie_id) {
     ];
 }
 
+// File up till this point only contains functions for supporting this feature:
+// Ability to browse and search for movies and see a profile page for them
+// OBJ RN: HANDLE WATCH LISTS FEATURE
 
+// NEED FUNCTIONS FOR ADDING AND REMOVING DATA TO WATCH LIST
+// ALSO NEED A GET FUNCTION TO RETRIVE THE WATCHLIST IF REQUESTED
+
+function addToWatch($user_id, $movie_id) {
+  $stmt = movieDB()->prepare(
+    "INSERT IGNORE INTO watchlist_movies (user_id, movie_id, date_added)
+     VALUES (?, ?, UNIX_TIMESTAMP())"
+  );
+  $stmt->bind_param("ii", $user_id, $movie_id);
+  $stmt->execute();
+  return ["ok" => true];
+}
 
 /*
 function for routing the requests that are coming through with their needed functions above
@@ -266,6 +281,8 @@ function requestProcessor($request) {
             return storeGenre((int)($request["tmdb_genre_id"] ?? 0), $request["name"] ?? "");
         case "store_movie":
             return storeMovie($request["movie"] ?? [], $request["genre_ids"] ?? []);
+        // STILL NEED TO ADD CASE STMTS FOR THE WATCHLIST FUNCTIONS
+
         default:
             return [
                 "ok" => false,
