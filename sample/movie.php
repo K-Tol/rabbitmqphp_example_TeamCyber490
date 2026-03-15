8d860b0fe7325b21a925ec9b9c2359bf1db9fb0d
Koleen_T <kt336@njit.edu>
2026-03-15 16:48:17 -0400

    cleaning up movies.php

--- sample/movie.php
+++ sample/movie.php
@@ -44,47 +44,6 @@
 
         $response["movies"];
 
-
-        switch ($request) {
-            case "login":
-                $response = $client->send_request([
-                    "type" => "login",
-                    "username" => $username,
-                    "password" => $password
-                ]);
-                if (!is_array($response) || empty($response["ok"])) {
-                    echo json_encode(["ok" => false, "message" => "login failed"]);
-                    exit(0);
-                }
-                $sessionKey = $response["session_key"] ?? "";
-                if ($sessionKey == "") {
-                    echo json_encode(["ok" => false, "message" => "no session key"]);
-                    exit(0);
-                }
-                setcookie("session_key", $sessionKey, [
-                    "expires" => time() + 86400,
-                    "path" => "/",
-                    "httponly" => true,
-                    ]);
-                echo json_encode(["ok" => true, "status" => "authorized", "message" => "login sucess"]);
-                exit(0);
-                break;
-            case "register":
-                $response = $client->send_request([
-                    "type" => "register",
-                    "username" => $username,
-                    "password" => $password
-                ]);
-                if (!is_array($response) || empty($response["ok"])) {
-                    echo json_encode(["ok" => false, "message" => "register failed"]);
-                    exit(0);
-                }
-                echo json_encode(["ok" => true, "status" => "registered", "message" => "registration successful"]);
-                break;
-            default:
-                echo json_encode(["ok" => false, "message" => "unsupported request type, politely FUCK OFF"]);
-                exit(0);
-        }
     } catch (Exception $e) {
         echo $e->getMessage();
 }
