7707aaaab4f708d336599d53f0b7ecf19320f0a3
Allen Zheng <az299@njit.edu>
2026-03-17 03:15:32 -0400

    made changes to storeMovie function

--- moviedb_listener.php
+++ moviedb_listener.php
@@ -82,21 +82,21 @@ function storeMovie($movie, $genre_ids = []) {
     );
 
     // this block will do the job of actually extracting movie data from the $movie array
-    $tmdb_id = isset($movie["tmdb_id"]) ? (int)$movie["tmdb_id"] : 0;
-    $title = isset($movie["title"]) ? (string)$movie["title"] : "";
-    $overview = array_key_exists("overview", $movie) ? $movie["overview"] : null;
-    $release_date = !empty($movie["release_date"]) ? $movie["release_date"] : null;
-    $runtime = isset($movie["runtime"]) && $movie["runtime"] !== null ? (int)$movie["runtime"] : null;
-    $poster_path = array_key_exists("poster_path", $movie) ? $movie["poster_path"] : null;
-    $backdrop_path = array_key_exists("backdrop_path", $movie) ? $movie["backdrop_path"] : null;
-    $original_language = array_key_exists("original_language", $movie) ? $movie["original_language"] : null;
-    $vote_average = isset($movie["vote_average"]) ? (float)$movie["vote_average"] : 0.0;
-    $vote_count = isset($movie["vote_count"]) ? (int)$movie["vote_count"] : 0;
-    $popularity = isset($movie["popularity"]) ? (float)$movie["popularity"] : 0.0;
+    $tmdb_id = (int)($movie["tmdb_id"] ?? 0);
+    $title = $movie["title"] ?? "";
+    $overview = $movie["overview"] ?? null;
+    $release_date = $movie["release_date"] ?? null;
+    $runtime = isset($movie["runtime"]) ? (int)$movie["runtime"] : null;
+    $poster_path = $movie["poster_path"] ?? null;
+    $backdrop_path = $movie["backdrop_path"] ?? null;
+    $original_language = $movie["original_language"] ?? null;
+    $vote_average = (float)($movie["vote_average"] ?? 0);
+    $vote_count = (int)($movie["vote_count"] ?? 0);
+    $popularity = (float)($movie["popularity"] ?? 0);
     $adult = !empty($movie["adult"]) ? 1 : 0;
 
     // binding the variables to their data types and then executing the insert
-    $stmt->bind_param("isssisssdddi", $tmdb_id, $title, $overview, $release_date, $runtime, $poster_path,
+    $stmt->bind_param("isssisssdidi", $tmdb_id, $title, $overview, $release_date, $runtime, $poster_path,
     $backdrop_path, $original_language, $vote_average, $vote_count, $popularity, $adult);
     $stmt->execute();
     // prepping query to find movie, binding the variable, then executing said query
