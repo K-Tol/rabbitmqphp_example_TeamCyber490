dc3c9b97f31795065d7c3b0367d037f4394ec5a7
Tom Paschalis <tp373@njit.edu>
2026-04-18 22:13:52 -0400

    added help check and 3rd variable to work with deployProcessor correctly

--- final_parts/Tom's_folder/installBundle.sh
+++ final_parts/Tom's_folder/installBundle.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/bash
+set -e
 
 bundlePath="$1"
 targetPath="$2"
+serviceName="$3"
+
+if [ "$bundlePath" = "h" ] || [ "$bundlePath" = "-h" ] || [ "$bundlePath" = "help" ] || [ "$bundlePath" = "-help" ]; then
+    echo "./installBundle.sh <bundlePath> <targetPath>"
+    exit
+fi
 
 mkdir -p "$targetPath"
 rm -rf "${targetPath:?}/"*
 tar -xf "$bundlePath" -C "$targetPath"
+
+systemctl restart "$serviceName"
