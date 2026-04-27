6b3d04a1ff6d51a865bba07d254d06d1d52a3fd3
Allen Zheng <az299@njit.edu>
2026-04-27 04:06:20 -0400

    added ssh and scp support

--- final_parts/Allen's_folder/buildBundle.sh
+++ final_parts/Allen's_folder/buildBundle.sh
@@ -32,3 +32,8 @@ echo "new" > "$stagingDir/status.txt"
 localBundle="$stagingDir/bundle.tar.gz"
 tar -czf "$localBundle" -C "stagingDir" .
 echo "Bundle created locally. Pushing to deployment VM via SCP..."
+
+versionDir="$deployDir/$name/$version"
+ssh -i ~/.ssh/id_ed25519 "${deployment_user}@${deployment_host}" "mkdir -p $versionDir"
+scp -i ~/.ssh/id_ed25519 "$localBundle" "${deployment_user}@${deployment_host}:$versionDir/bundle.tar.gz"
+echo "Bundle has been successfully pushed to ${deployment_user}@${deployment_host}:$versionDir/bundle.tar.gz"
