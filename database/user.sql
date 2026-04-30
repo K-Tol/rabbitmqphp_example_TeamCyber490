288b8919d78e4f35fd51bbfc79e3e5ca967723b4
Allen Zheng <az299@njit.edu>
2026-04-30 00:10:12 -0400

    added if not exist stmt for user.sql

--- database/user.sql
+++ database/user.sql
@@ -3,7 +3,7 @@ RUN THIS FILE ALONG WITH "setup.sql" IF YOU WANT TO TEST OUT THE DATABASE ON YOU
 */
 
 -- creating a local database user so it can access our database locally and grants it with permissions to do everything
-CREATE USER 'db_user'@'localhost' IDENTIFIED BY 'passwd123';
+CREATE USER IF NOT EXISTS 'db_user'@'localhost' IDENTIFIED BY 'passwd123';
 GRANT SELECT, INSERT, UPDATE, DELETE ON auth_db.* TO 'db_user'@'localhost';
 GRANT SELECT, INSERT, UPDATE, DELETE ON movie_db.* TO 'db_user'@'localhost';
 FLUSH PRIVILEGES;
